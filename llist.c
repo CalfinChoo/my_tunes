1e2dd3f325c5e59e05074b99e1bb069f84277603
Anthony Li <ali04@stuy.edu>
2019-10-30 10:38:47 -0400

    more classwork

--- llist.c
+++ llist.c
@@ -10,9 +10,9 @@ void print_list(struct node * p) {
   if (p != NULL) {
     printf("%s: %s |", p->artist, p->name);
     struct node n;
-    n = *p;
-    while (n.next != NULL) {
-      n = *(n.next);
+    n = p;
+    while (n->next != NULL) {
+      n = n->next;
       printf("%s: %s |", n->artist, n->name);
     }
   }
@@ -26,48 +26,98 @@ struct node * insert_front(struct node * p, char * a, char * n) {
   return q;
 }
 struct node * insert(struct node * p, char * a, char * n) {
+  if (!p) insert_front(p, a, n);
   if (strcmp(a, p->artist) < 0 && strcmp(n, p->name) < 0){
     insert_front(p, a, n);
     return;
   }
+  struct node * l = p;
+  struct node * l2 = p->next;
   struct node * q = malloc(sizeof(struct node));
   q->artist = a;
   q->name = n;
-  while (q.next != NULL) {
-    q = *(q.next);
-    if (strcmp(a, q->artist) < 0){
-
+  while (l2) {
+    if (strcmp(a, l2->artist) == 0){
+      while (strcmp(n, l2->name) > 0) {
+        l = l->next;
+        l2 = l2->next;
+      }
+      q->next = l2;
+      l->next = q;
+      return p;
     }
+    if (strcmp(a, l2->artist) > 0){
+      q->next = l2;
+      l->next = q;
+      return p;
+    }
+    l = l->next;
+    l2 = l2->next;
   }
+  l->next = q;
+  q->next = NULL;
   return p;
+}
+struct node * song(struct node * p, char * a, char * n){
+  struct node * temp = p;
+  while (temp){
+    if (strcmp(a, temp->artist) == 0 && strcmp(n, temp->name) == 0) return temp;
+    temp = temp->next;
+  }
+  return NULL;
+}
+struct node * artist(struct node * p, char * a){
+  struct node * temp = p;
+  while (temp){
+    if (strcmp(a, temp->artist) == 0) return temp;
+    temp = temp->next;
+  }
+  return NULL;
+}
+int size(struct node * p){
+  int x = 0;
+  struct node * temp = p;
+  while (temp){
+    x++;
+    temp = temp->next;
+  }
+  return x;
+}
+struct node * random_song(struct node * p){
+  int x = rand() % size(p);
+  struct node * temp = p;
+  for (; x > 0; x--){
+    temp = temp->next;
+  }
+  return temp;
 }
 struct node * free_list(struct node * p) {
   struct node * current;
   current = p->next;
   if (current != NULL) {
-    printf("\nfreeing node: %d", p->i);
+    printf("\nfreeing node: %s : %s", p->artist, p->name);
     free_list(current);
   }
   free(p);
   p = NULL;
   return p;
 }
-struct node * rem(struct node *front, int data) {
+struct node * rem(struct node *front, char * a, char * n) {
   struct node * current;
   current = front->next;
-  if (front->i == data) {
+  if (strcmp(front->artist, a) == 0 && strcmp(front->name, n) == 0) {
     free(front);
     front = NULL;
     return current;
   }
   while (current != NULL) {
-    if (current->i == data) {
+    if (strcmp(front->artist, a) == 0 && strcmp(front->name, n) == 0) {
       front->next = current->next;
       free(current);
       current = NULL;
       return front;
     }
-    else rem(current, data);
+    else rem(current, a, n);
     return front;
   }
   return front;
--- main.c
+++ main.c
@@ -4,19 +4,12 @@ int main() {
   struct node * list = linked_list();
   printf("Printing empty list:");
   print_list(list);
-  printf("\n\nAdding #s 0-9 to list.");
-  int i;
-  for (i = 0; i < 10; i++) {
-    list = insert_front(list, i);
-  }
+  printf("\n\nAdding song to list.");
+  list = insert(list, "abc", "dtl");
   printf("\n\nPrinting list:");
   print_list(list);
   printf("\n\nRemoving 10.");
-  list = rem(list, 10);
-  printf("\n\nPrinting list:");
-  print_list(list);
-  printf("\n\nRemoving 5.");
-  list = rem(list, 5);
+  list = rem(list, "abc", "dtl");
   printf("\n\nPrinting list:");
   print_list(list);
   printf("\n\nFreeing list.");
